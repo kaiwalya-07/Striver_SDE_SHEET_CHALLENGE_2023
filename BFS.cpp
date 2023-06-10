d3c5048b558c5f69a10081e766169f90078de11b
Kaiwalya Dudhgaonkar <82649070+kaiwalya-07@users.noreply.github.com>
2023-06-10 08:30:15 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the BinaryTreeNode class structure
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+    //  Write your code here.
+    queue<BinaryTreeNode<int>*> q;
+    vector<int> ans;
+    if(root==NULL) return ans;
+    
+    q.push(root);
+    
+
+    while(!q.empty()){
+        int s=q.size();
+
+        while(s--){
+            BinaryTreeNode<int>*node=q.front();
+            q.pop();
+            ans.push_back(node->val);
+            if(node->left!=NULL) q.push(node->left);
+            if(node->right!=NULL) q.push(node->right);
+            
+        }
+    }
+
+    return ans;  
+}
+
+
